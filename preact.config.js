b36d027da4b713a63697adc7614e8aa6535fee68
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2020-11-12 21:18:43 -0500

    Move preconnect tag to template

--- preact.config.js
+++ preact.config.js
@@ -1,12 +1,7 @@
-import PreconnectHtmlWebpackPlugin from "preconnect-html-webpack-plugin";
 import CopyPlugin from "copy-webpack-plugin";
 
-export default (config, env, helpers) => {
+export default (config) => {
   config.output.publicPath = "/twitter/cjtf_nj/";
-
-  config.plugins.push(
-    new PreconnectHtmlWebpackPlugin(["https://pbs.twimg.com"])
-  );
 
   config.plugins.push(
     new CopyPlugin({
